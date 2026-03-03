171760ccf6bb34ff4ec8cbf66231f3de7f9b2ba2
Balast41 <qbalazot@gmail.com>
2026-03-03 15:16:54 +0100

    Ajout de la mémoisation & Elagage alpha|beta

--- Othello.py
+++ Othello.py
@@ -4,6 +4,12 @@ import numpy as np
 import time
 import copy
 
+table_transposition={}
+
+def plateau_to_cle(p):
+    return tuple(tuple(row) for row in p)
+
+
 def afficher_plateau(p):
     for l in p:
         print(" ".join([c if c!="" else "." for c in l]))
@@ -133,16 +139,97 @@ def minimax(p, profondeur, joueur):
                     best = min(best, minimax(pcopie, profondeur-1, "X"))
         return best if coups_trouves else h(p)
     
+
+def minimax_alpha_beta(p, profondeur, joueur, alpha=-np.inf, beta=np.inf, memo=None):
+    if memo is None:
+        memo={}
+
+    cle = (plateau_to_cle(p), joueur, profondeur)
+    if cle in memo:
+        return memo[cle]
+    
+    if profondeur == 0:
+        score = h(p)
+        memo[cle] = score
+        return score
+    
+    coups_trouves = False
+    
+    if joueur == "X":
+        best= -np.inf
+        for x in range(8):
+            for y in range(8):
+                if p[x][y] == "" and coup_jouable(p,x,y, "X"):
+                    coups_trouves = True
+                    pcopie = copy.deepcopy(p)
+                    retournement(pcopie, "X",x,y)
+                    best = max(best, minimax_alpha_beta(pcopie, profondeur-1, "O",alpha,beta,memo))
+                    alpha = max(alpha, best)
+                    if beta <= alpha:
+                        break
+            if beta <= alpha:
+                break
+        score= best if coups_trouves else h(p)
+    else:
+        best= np.inf
+        for x in range(8):
+            for y in range(8):
+                if p[x][y] == "" and coup_jouable(p,x,y, "O"):
+                    coups_trouves = True
+                    pcopie = copy.deepcopy(p)
+                    retournement(pcopie, "O",x,y)
+                    best = min(best, minimax_alpha_beta(pcopie, profondeur-1, "X",alpha,beta,memo))
+                    alpha = min(alpha, best)
+                    if beta <= alpha:
+                        break
+            if beta <= alpha:
+                break
+        score= best if coups_trouves else h(p)
+    
+    memo[cle]=score
+    return score
+
+
+def choisir_coup_memo(p, joueur, profondeur=2,memo=None):
+    
+    if memo is None:
+        memo={}
+
+    meilleur_score = -np.inf if joueur == "X" else np.inf
+    meilleur_coup = None
+    for x in range(8):
+        for y in range(8):
+            if p[x][y] == "" and coup_jouable(p, x, y, joueur):
+                pcopie = copy.deepcopy(p)
+                retournement(pcopie, joueur, x, y)
+
+                score = minimax_alpha_beta(pcopie,profondeur-1,"O" if joueur == "X" else "X",memo=memo)
+
+                if joueur == "X":
+                    if score > meilleur_score:
+                        meilleur_score = score
+                        meilleur_coup = (x, y)
+                else:
+                    if score < meilleur_score:
+                        meilleur_score = score
+                        meilleur_coup = (x, y)
+
+    return meilleur_coup, memo
+    
 def main():
     p = initialisation_plateau()
     joueur = "X"
     passes = 0
+    memo_global = {}
 
     while True:
         afficher_plateau(p)
-        time.sleep(1)
+        
+        tic= time.time()
+        coup, memo_global = choisir_coup_memo(p, joueur, profondeur=1,memo=memo_global)
+        tac = time.time() - tic
 
-        coup = choisir_coup(p, joueur, profondeur=1)
+        print("Temps de calcul : ", tac," s | taille du cache : ", len(memo_global))
 
         if coup is None:
             print("Pas de coup pour", joueur)
